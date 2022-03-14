a411c6c4feba96d46f07a40cee0fb8359a4f4659
Kyrylo Riabov <kyryl.ryabov@gmail.com>
2022-03-14 21:20:01 +0200

    Add show, showAll and Delete handlers for all models

--- src/main/java/ua/site/controllers/crud/PeopleController.java
+++ src/main/java/ua/site/controllers/crud/PeopleController.java
@@ -1,74 +1,63 @@
 package ua.site.controllers.crud;
 
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.*;
-import ua.site.dao.crud.PersonDAO;
-import ua.site.models.crud.Person;
-
-import javax.validation.Valid;
-
-
-@Controller
-@RequestMapping("/people")
+//@Controller
+//@RequestMapping("/people")
 public class PeopleController {
 
-    private final PersonDAO personDAO;
-
-    @Autowired
-    public PeopleController(PersonDAO personDAO) {
-        this.personDAO = personDAO;
-    }
-
-    @GetMapping()
-    public String index(Model model) {
-        model.addAttribute("people", personDAO.index());
-        return "crud/people/index";
-    }
-
-    @GetMapping("/{id}")
-    public String show(@PathVariable("id") int id, Model model) {
-        model.addAttribute("person", personDAO.show(id));
-        return "crud/people/show";
-    }
-
-    @GetMapping("/new")
-    public String newPerson(@ModelAttribute("person") Person person) {
-        return "crud/people/new";
-    }
-
-    @PostMapping()
-    public String create(@ModelAttribute("person") @Valid Person person,
-                         BindingResult bindingResult) {
-        if (bindingResult.hasErrors())
-            return "crud/people/new";
-
-        personDAO.save(person);
-        return "redirect:/people";
-    }
-    @GetMapping("/{id}/edit")
-    public String edit(Model model,
-                       @PathVariable("id") int id) {
-        model.addAttribute("person", personDAO.show(id));
-        return "crud/people/edit";
-    }
-
-    @PatchMapping("/{id}")
-    public String update(@ModelAttribute("person") @Valid Person person, BindingResult bindingResult,
-                         @PathVariable("id") int id) {
-        if (bindingResult.hasErrors())
-            return "crud/people/edit";
-
-        personDAO.update(id, person);
-        return "redirect:/people";
-    }
-
-    @DeleteMapping("/{id}")
-    public String delete(@PathVariable("id") int id) {
-        personDAO.delete(id);
-        return "redirect:/people";
-    }
+//    private final PersonDAO personDAO;
+//
+//    @Autowired
+//    public PeopleController(PersonDAO personDAO) {
+//        this.personDAO = personDAO;
+//    }
+//
+//    @GetMapping()
+//    public String index(Model model) {
+//        model.addAttribute("people", personDAO.index());
+//        return "crud/people/index";
+//    }
+//
+//    @GetMapping("/{id}")
+//    public String show(@PathVariable("id") int id, Model model) {
+//        model.addAttribute("person", personDAO.show(id));
+//        return "crud/people/show";
+//    }
+//
+//    @GetMapping("/new")
+//    public String newPerson(@ModelAttribute("person") Person person) {
+//        return "crud/people/new";
+//    }
+//
+//    @PostMapping()
+//    public String create(@ModelAttribute("person") @Valid Person person,
+//                         BindingResult bindingResult) {
+//        if (bindingResult.hasErrors())
+//            return "crud/people/new";
+//
+//        personDAO.save(person);
+//        return "redirect:/people";
+//    }
+//    @GetMapping("/{id}/edit")
+//    public String edit(Model model,
+//                       @PathVariable("id") int id) {
+//        model.addAttribute("person", personDAO.show(id));
+//        return "crud/people/edit";
+//    }
+//
+//    @PatchMapping("/{id}")
+//    public String update(@ModelAttribute("person") @Valid Person person, BindingResult bindingResult,
+//                         @PathVariable("id") int id) {
+//        if (bindingResult.hasErrors())
+//            return "crud/people/edit";
+//
+//        personDAO.update(id, person);
+//        return "redirect:/people";
+//    }
+//
+//    @DeleteMapping("/{id}")
+//    public String delete(@PathVariable("id") int id) {
+//        personDAO.delete(id);
+//        return "redirect:/people";
+//    }
 }
