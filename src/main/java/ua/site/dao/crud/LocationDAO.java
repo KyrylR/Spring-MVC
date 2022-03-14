00b575c9d0603ca9a4e13b5ef927a9c55bd68fb7
Kyrylo Riabov <kyryl.ryabov@gmail.com>
2022-03-14 13:35:22 +0200

    Clean DAO

--- src/main/java/ua/site/dao/crud/LocationDAO.java
+++ src/main/java/ua/site/dao/crud/LocationDAO.java
@@ -5,7 +5,7 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 import ua.site.models.crud.Area;
 import ua.site.models.crud.Field;
-import ua.site.models.crud.Test;
+import ua.site.models.crud.Sample;
 
 import java.util.List;
 
@@ -17,34 +17,45 @@ public class LocationDAO {
         this.jdbcTemplate = jdbcTemplate;
     }
 
-    public Field showField(int id) {
-        return jdbcTemplate.query("SELECT * FROM field WHERE id=?", new Object[]{id},
-                        new BeanPropertyRowMapper<>(Field.class))
-                .stream().findAny().orElse(null);
-    }
-
-    public Area showArea(int id) {
-        return jdbcTemplate.query("SELECT * FROM area WHERE id=?", new Object[]{id},
-                        new BeanPropertyRowMapper<>(Area.class))
-                .stream().findAny().orElse(null);
-    }
-
-    public Test showTest(int id) {
-        return jdbcTemplate.query("SELECT * FROM test WHERE id=?", new Object[]{id},
-                        new BeanPropertyRowMapper<>(Test.class))
-                .stream().findAny().orElse(null);
-    }
-
-    public List<Field> indexFields() {
-        return jdbcTemplate.query("SELECT * FROM field", new BeanPropertyRowMapper<>(Field.class));
-    }
-
-    public List<Test> indexTests() {
-        return jdbcTemplate.query("SELECT * FROM test", new BeanPropertyRowMapper<>(Test.class));
-    }
-
+    // ------------ Area block --------------
     public List<Area> indexAreas() {
         return jdbcTemplate.query("SELECT * FROM area", new BeanPropertyRowMapper<>(Area.class));
+    }
+
+    public void saveArea(Area area) {
+        jdbcTemplate.update("INSERT INTO area (region) VALUES(?)",
+                area.getRegion());
+    }
+
+    public void update(int id, Area updatedArea) {
+        jdbcTemplate.update("UPDATE area SET region=? WHERE id=?", updatedArea.getRegion(), id);
+    }
+
+    public void delete(int id) {
+        jdbcTemplate.update("DELETE FROM area WHERE id=?", id);
+    }
+
+
+    public List<Field> indexFields() {
+        return jdbcTemplate.query("SELECT * FROM field", (rs, rowNum) -> {
+            Field field = new Field();
+            field.setId(rs.getInt("id"));
+            field.setName(rs.getString("name"));
+            field.setLatitude((Double[]) rs.getArray("latitude").getArray());
+            field.setLongitude((Double[]) rs.getArray("longitude").getArray());
+            return field;
+        });
+    }
+
+    public List<Sample> indexTests() {
+        return jdbcTemplate.query("SELECT * FROM test", (rs, rowNum) -> {
+            Sample sample = new Sample();
+            sample.setId(rs.getInt("id"));
+            sample.setLatitude(rs.getDouble("latitude"));
+            sample.setLongitude(rs.getDouble("longitude"));
+            sample.setField(new Field());
+            return sample;
+        });
     }
 
     public void saveField(Field field) {
@@ -52,22 +63,33 @@ public class LocationDAO {
                 field.getLatitude(), field.getLongitude());
     }
 
-    public void saveArea(Area area) {
-        jdbcTemplate.update("INSERT INTO area (region, fk_field) VALUES(?, (SELECT id from field WHERE id=?))",
-                area.getRegion(), area.getField().getId());
-    }
-
-    public void saveTest(Test test) {
+    public void saveTest(Sample sample) {
         jdbcTemplate.update("INSERT INTO test (latitude, longitude, fk_field) VALUES(?, ?, (SELECT id from field WHERE id=?))",
-                test.getLatitude(), test.getLongitude(), test.getField().getId());
+                sample.getLatitude(), sample.getLongitude(), sample.getField().getId());
     }
 
-//    public void update(int id, Person updatedPerson) {
-//        jdbcTemplate.update("UPDATE Person SET name=?, age=?, email=? WHERE id=?", updatedPerson.getName(),
-//                updatedPerson.getAge(), updatedPerson.getEmail(), id);
+    //    public Field showField(int id) {
+//        return jdbcTemplate.query("SELECT * FROM field WHERE id=?", new Object[]{id}, (rs, rowNum) -> {
+//                            Field field = new Field();
+//                            field.setId(rs.getInt("id"));
+//                            field.setName(rs.getString("name"));
+//                            field.setLatitude((Double[]) rs.getArray("latitude").getArray());
+//                            field.setLongitude((Double[]) rs.getArray("longitude").getArray());
+//                            return field;
+//                        })
+//                .stream().findAny().orElse(null);
 //    }
 //
-//    public void delete(int id) {
-//        jdbcTemplate.update("DELETE FROM Person WHERE id=?", id);
+//    public Area showArea(int id) {
+//        return jdbcTemplate.query("SELECT * FROM area WHERE id=?", new Object[]{id},
+//                        new BeanPropertyRowMapper<>(Area.class))
+//                .stream().findAny().orElse(null);
 //    }
+//
+//    public Sample showTest(int id) {
+//        return jdbcTemplate.query("SELECT * FROM test WHERE id=?", new Object[]{id},
+//                        new BeanPropertyRowMapper<>(Sample.class))
+//                .stream().findAny().orElse(null);
+//    }
+
 }
