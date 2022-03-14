bc5b8f3c5fb96c89fcdc8a1c227b8971b5fc659f
Kyrylo Riabov <kyryl.ryabov@gmail.com>
2022-03-14 21:17:03 +0200

    Add getters for Fields and Samples

--- src/main/java/ua/site/services/ModelService.java
+++ src/main/java/ua/site/services/ModelService.java
@@ -3,9 +3,11 @@ package ua.site.services;
 import org.springframework.stereotype.Service;
 import ua.site.dao.crud.LocationDAO;
 import ua.site.models.crud.Area;
+import ua.site.models.crud.Field;
 import ua.site.models.crud.Paging.Page;
 import ua.site.models.crud.Paging.Paged;
 import ua.site.models.crud.Paging.Paging;
+import ua.site.models.crud.Sample;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -14,14 +16,37 @@ import java.util.stream.Collectors;
 public class ModelService {
     // ------------ Area block --------------
     public Paged<Area> getAreas(int pageNumber, int size, LocationDAO locationDAO) {
-        List<Area> areas = locationDAO.indexAreas();
+        List<Area> areas = locationDAO.indexArea();
 
         List<Area> paged = areas.stream()
-                .skip(pageNumber)
                 .limit(size)
                 .collect(Collectors.toList());
 
         int totalPages = areas.size() / size;
+        return new Paged<>(new Page<>(paged, totalPages), Paging.of(totalPages, pageNumber, size));
+    }
+
+    // ------------ Field block --------------
+    public Paged<Field> getFields(int pageNumber, int size, LocationDAO locationDAO) {
+        List<Field> fields = locationDAO.indexField();
+
+        List<Field> paged = fields.stream()
+                .limit(size)
+                .collect(Collectors.toList());
+
+        int totalPages = fields.size() / size;
+        return new Paged<>(new Page<>(paged, totalPages), Paging.of(totalPages, pageNumber, size));
+    }
+
+    // ------------ Sample block --------------
+    public Paged<Sample> getSamples(int pageNumber, int size, LocationDAO locationDAO) {
+        List<Sample> samples = locationDAO.indexSample();
+
+        List<Sample> paged = samples.stream()
+                .limit(size)
+                .collect(Collectors.toList());
+
+        int totalPages = samples.size() / size;
         return new Paged<>(new Page<>(paged, totalPages), Paging.of(totalPages, pageNumber, size));
     }
 }
