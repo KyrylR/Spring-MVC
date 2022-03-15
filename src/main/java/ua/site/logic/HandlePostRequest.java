d5d37bbcc46031928d958a80adcf186c6972569a
Kyrylo Riabov <kyryl.ryabov@gmail.com>
2022-03-15 20:59:39 +0200

    Create handler for post request with json file

---
+++ src/main/java/ua/site/logic/HandlePostRequest.java
@@ -0,0 +1,36 @@
+package ua.site.logic;
+
+import ua.site.dao.crud.LocationDAO;
+import ua.site.models.crud.Area;
+import ua.site.models.crud.Field;
+import ua.site.models.crud.Location.Location;
+import ua.site.models.crud.Sample;
+
+import java.util.List;
+
+public class HandlePostRequest {
+    public static String handle(Location[] locations, LocationDAO locationDAO) {
+        StringBuilder result = new StringBuilder();
+        for (Location location : locations) {
+            String region = location.getRegion();
+            double lon = location.getX();
+            double lat = location.getY();
+            Area area = locationDAO.findAreaByRegion(region);
+            if (area == null) {
+                result.append("Location with region: '").append(region).append("' skipped, no such region in DB!\n");
+                continue;
+            }
+            Sample sample = new Sample(lat, lon, area.getId());
+            List<Field> fields = locationDAO.findFieldByAreaId(sample.getAreaId());
+            Field field = SampleToFieldMatcher.match(fields, sample);
+            if (field == null) {
+                result.append("Location with region: '").append(region).append("' skipped, because sample coordinates do not match any field in DB!\n");
+                continue;
+            }
+            sample.setField(field);
+            locationDAO.saveSample(sample);
+            result.append("Location with region: '").append(region).append("' added to DB!\n");
+        }
+        return result.toString();
+    }
+}
