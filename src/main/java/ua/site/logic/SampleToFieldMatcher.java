f59513b76f538e635d365197e8edf23a2500d5c4
Kyrylo Riabov <kyryl.ryabov@gmail.com>
2022-03-15 15:30:51 +0200

    Make logic to match coordinates and field

---
+++ src/main/java/ua/site/logic/SampleToFieldMatcher.java
@@ -0,0 +1,30 @@
+package ua.site.logic;
+
+import ua.site.models.crud.Field;
+import ua.site.models.crud.Location.Location;
+import ua.site.models.crud.Location.PointLocation;
+import ua.site.models.crud.Sample;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SampleToFieldMatcher {
+    public static Field match(List<Field> fields, Sample sample) {
+        for (Field field : fields) {
+            Double[] lat = field.getLatitude();
+            Double[] lon = field.getLongitude();
+            List<Location> locations = new ArrayList<>();
+            String possibleRegion = field.getArea().getRegion();
+            for (int i = 0; i < lat.length; i++) {
+                locations.add(new Location(possibleRegion, lat[i], lon[i]));
+            }
+            Location test = new Location(possibleRegion, sample.getLatitude(), sample.getLongitude());
+            var temp = locations.toArray(new Location[0]);
+            boolean res = PointLocation.isInside(test, temp);
+            if (res) {
+                return field;
+            }
+        }
+        return null;
+    }
+}
